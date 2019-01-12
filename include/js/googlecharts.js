1f5f5985410c9a9bf6388820207873b31672075a
liorsh <liorsh@shenkar.ac.il>
2019-01-12 12:54:46 +0200

    font sizr

--- include/js/googlecharts.js
+++ include/js/googlecharts.js
@@ -1,8 +1,9 @@
 $(window).on("load", function() {
   google.charts.load("current", { packages: ["bar"] });
-  google.charts.setOnLoadCallback(drawChart);
+  google.charts.load("current", { packages: ["corechart"] });
+  /*   google.charts.setOnLoadCallback(drawChart);
 
-  function drawChart() {
+   function drawChart() {
     var jsonData = $.ajax({
       url: "https://yourslangapi.herokuapp.com/getTvShowRoutes",
       dataType: "json", // type of data we're expecting from server
@@ -15,13 +16,14 @@ $(window).on("load", function() {
       chart: {
         title: "כמות ציוצים לתוכנית"
       },
+
       bars: "horizontal" // Required for Material Bar Charts.
     };
     var chart = new google.charts.Bar(
       document.getElementById("barchart_material")
     );
     chart.draw(data, google.charts.Bar.convertOptions(options));
-  }
+  } */
 
   $(".col-lg-3.tvShow").on("click", function(event) {
     let parentDiv = event.target.parentNode;
@@ -32,7 +34,7 @@ $(window).on("load", function() {
 });
 
 function drawChartByName(name) {
-  var jsonData = $.ajax({
+  /* var jsonData = $.ajax({
     url: "https://yourslangapi.herokuapp.com/getTvShowByNameRoutes/" + name,
     dataType: "json", // type of data we're expecting from server
     async: false // make true to avoid waiting for the request to be complete
@@ -42,6 +44,9 @@ function drawChartByName(name) {
   var dataToDisplay = [];
   dataToDisplay.push(["ביטוי", "כמות ציוצים"]);
   jsonData.responseJSON[0].sentences.forEach(item => {
+    let barColor;
+
+    console.log(barColor);
     dataToDisplay.push([item.text, item.tweets.length]);
   });
   console.log(dataToDisplay);
@@ -53,7 +58,74 @@ function drawChartByName(name) {
     },
     bars: "horizontal" // Required for Material Bar Charts.
   };
+
   var chart = new google.charts.Bar(
+    document.getElementById("barchart_material")
+  );
+  chart.draw(data, google.charts.Bar.convertOptions(options)); */
+  var jsonData = $.ajax({
+    url: "https://yourslangapi.herokuapp.com/getTvShowByNameRoutes/" + name,
+    dataType: "json", // type of data we're expecting from server
+    async: false // make true to avoid waiting for the request to be complete
+  });
+  //console.log(jsonData.responseJSON);
+  let tvShowName = jsonData.responseJSON[0].tvshowname;
+  console.log("tvShowName " + tvShowName);
+  let dataToDisplay = [];
+  dataToDisplay.push(["", "", { role: "style" }]);
+  jsonData.responseJSON[0].sentences.forEach(item => {
+    let color;
+    if (item.tweets.length <= 5) {
+      color = "red";
+    } else if (item.tweets.length > 5 && item.tweets.length <= 12) {
+      color = "orange";
+    } else {
+      color = "green";
+    }
+    dataToDisplay.push([item.text, item.tweets.length, color]);
+  });
+
+  var data = google.visualization.arrayToDataTable(dataToDisplay);
+
+  var options = {
+    chart: {
+      title: tvShowName,
+      fontSize: 22,
+      fontName: "Arial"
+    },
+    hAxis: {
+      title: "כמות ציוצים",
+      titleTextStyle: {
+        fontSize: 22,
+        fontName: "Arial"
+      },
+
+      textStyle: {
+        fontSize: 20
+      }
+    },
+    vAxis: {
+      title: "ביטוי",
+      titleTextStyle: {
+        fontSize: 22,
+        fontName: "Arial"
+      },
+      maxValue: 15,
+      textStyle: {
+        fontSize: 20,
+        paddingRight: "200",
+        marginRight: "200",
+        paddingTop: "100",
+        marginTop: "100"
+      }
+    },
+    height: 1000,
+    width: 1000,
+    legend: { position: "none" },
+    chartArea: { left: 400, width: "80%" }
+  };
+
+  var chart = new google.visualization.BarChart(
     document.getElementById("barchart_material")
   );
   chart.draw(data, google.charts.Bar.convertOptions(options));
